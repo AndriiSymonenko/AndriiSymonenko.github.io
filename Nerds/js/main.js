49e45c11a404cf10dc00fbd44350653595770fb6
Andrii <andrii.symonenko.dev@gmail.com>
2020-01-20 22:16:38 +0200

    add slider, catalog page

--- Nerds/js/main.js
+++ Nerds/js/main.js
@@ -1,16 +1,39 @@
-let mailUsButton = document.querySelector('.contact-modal-button');
-let mailUsPopUp = document.querySelector('.modal');
-let closeModalButton = document.querySelector('.button-close-modal')
+let mailUsButton = document.querySelector(".contact-modal-button");
+let mailUsPopUp = document.querySelector(".modal");
+let closeModalButton = document.querySelector(".button-close-modal");
 
+function sliderControlActive() {
+  let sliderControl = document.querySelectorAll(".slider-button");
+  let sliderContent = document.querySelectorAll(".slider-item");
+  let controlName;
+  sliderControl.forEach(item => {
+    item.addEventListener("click", selectSliderControl);
+  });
+  function selectSliderControl() {
+    sliderControl.forEach(item => {
+      item.classList.remove("active");
+    });
+    this.classList.add("active");
+    controlName = this.getAttribute("data-control-name");
+    selectControlName(controlName);
+  }
+  function selectControlName() {
+    sliderContent.forEach(item => {
+      item.classList.contains(controlName)
+        ? item.classList.add("is-active")
+        : item.classList.remove("is-active");
+    });
+  }
+}
 
+sliderControlActive();
 
-mailUsButton.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    mailUsPopUp.classList.add("modal-show");
+mailUsButton.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  mailUsPopUp.classList.add("modal-show");
 });
 
-
-closeModalButton.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    mailUsPopUp.classList.remove("modal-show");
+closeModalButton.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  mailUsPopUp.classList.remove("modal-show");
 });
