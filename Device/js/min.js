e09a6b04e3161871189764cce6e39c1fb0bff4cd
Andrii <andrii.symonenko.dev@gmail.com>
2020-03-09 22:22:50 +0200

    fix servicce-slider, add color icom brands

--- Device/js/min.js
+++ Device/js/min.js
@@ -1,2 +1,9 @@
 let sliderControl = document.querySelectorAll(".slider-control");
-let sliderContent = document.querySelectorAll("slider-item");
+let sliderContent = document.querySelectorAll(".slide-item");
+
+sliderControl[1].addEventListener('click', function () {
+    sliderControl[0].classList.remove('active-control');
+    sliderContent[0].classList.remove('is-active');
+    sliderControl[1].classList.add('active-control');
+    sliderContent[1].classList.add('is-active');
+});
