77b6dccfe79e2e1835d866a5fc0cef509337f223
Andrii <andrii.symonenko.dev@gmail.com>
2020-03-14 22:21:25 +0200

    add result element, fix markup

--- Alcoholic/js/min.js
+++ Alcoholic/js/min.js
@@ -2,15 +2,17 @@ let quantityAlcohol = document.querySelector('.quantity-alcohol');
 let percentAlcohol = document.querySelector('.percent-alcohol');
 let checkButton = document.querySelector('.check-button');
 let checkResult = document.querySelector('.check-result');
-let resultAlcohol = document.querySelector('result-alcohol');
-let resultTextVodka = document.querySelector('result-vodka');
+let resultTextAlcohol = document.querySelector('.result-alcohol');
+let resultTextVodka = document.querySelector('.result-vodka');
 
 checkButton.onclick = function () {
     let resultCleanAlcohol = (percentAlcohol.value / 100) * (quantityAlcohol.value * 1000);
     let resultVodka = resultCleanAlcohol * 100 / 40;
 
-    // checkResult.classList.remove('hidden');
-    // checkButton.classList.add('hidden');
-    alert('Водки: ' + resultVodka + ' ' + 'Спирта: ' + resultCleanAlcohol);
-    console.log(resultVodka, resultCleanAlcohol);
+
+    checkResult.classList.remove('hidden');
+    checkButton.classList.add('hidden');
+
+    resultTextVodka.innerHTML = 'Водки: ' + resultVodka.toFixed(1) + ' ' + 'мл';
+    resultTextAlcohol.innerHTML = 'Спирта: ' + resultCleanAlcohol.toFixed(1) + ' ' + 'мл';
 }
