2f4efa8f02148b0a3f8adbb3cec51f87b00825e6
Andrii Symonenko <andrii.symonenko.dev@gmail.com>
2020-05-03 20:47:30 +0300

    add some style, and js

--- Landing Page/js/script.js
+++ Landing Page/js/script.js
@@ -1,5 +1,20 @@
-let priceButton = document.getElementsByClassName('button main-price__button'),
+let priceList = document.querySelector('.main-price__list'),
+    priceButton = document.querySelectorAll('.main-price__button'),
     priceText = document.querySelectorAll('.main-price__price');
 
-    console.log(priceButton[0]);
-    console.log(priceText);
+
+    priceList.addEventListener('mouseover', function(event) {
+        for (let i = 0; i < priceText.length; i++) {
+            if (event.target.classList.contains('main-price__button') && event.target == priceButton[i]) {
+                priceText[i].style.color = "#112d4e";
+            } 
+        }
+    });
+
+    priceList.addEventListener('mouseout', function(event) {
+        for (let i = 0; i < priceText.length; i++) {
+            if (event.target.classList.contains('main-price__button') && event.target == priceButton[i]) {
+                priceText[i].style.color = "#3f72af";
+            } 
+        }
+    });
